62c3dca2e14a3674717a3b2cbeaca7a6c34b2b38
Cristiane11 <bjwmcd@yahoo.com>
2020-07-04 03:58:49 -0400

    update

---
+++ pink-purple/src/components/ui/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+ function Header(props){
+    return
+        (
+        <AppBar>
+            <Toolbar>
+                Pink & Purple
+            </Toolbar>
+        </AppBar>
+     )
+    
+}
+export default Header;
